f832bc5d55c6af4189ea113164afec6a40ac456b
k2008siru-star <k2008siru@gmail.com>
2026-08-14 11:14:30 +0900

    プレイヤー検知

--- Assets/Script/Enemy/EnemyData.cs
+++ Assets/Script/Enemy/EnemyData.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class EnemyData : MonoBehaviour
 {
-    public float enemy_test_hp;
-    public float enemy_test_attack;
-    public float enemy_test_spped;
-    public float enemy_test_detect;
-    public float enemy_test_arm;
-    public float enemy_test_cooltime;
+    public float enemyHp;
+    public float enemyAttack;
+    public float enemySpped;
+    public float enemyDetect;
+    public float enemyArm;
+    public float enemyCooltime;
+    public bool isInDetect;
+    public GameObject player;
+    private float distanceX;
+    private float distanceY;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        isInDetect = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //索敵範囲内かの判定
+        if(!isInDetect)
+        {
+            distanceX = transform.position.x - player.transform.position.x;
+            distanceY = transform.position.y - player.transform.position.y;
+            if(distanceX*distanceX + distanceY*distanceY < enemyDetect*enemyDetect)
+            {
+                isInDetect = true;
+            }
+        }
     }
 }
