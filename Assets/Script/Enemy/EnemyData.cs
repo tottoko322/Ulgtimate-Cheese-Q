cf46666cea4e49d23095b8aa793e71edb5e44e46
k2008siru-star <k2008siru@gmail.com>
2026-08-14 10:21:13 +0900

    EnemyData変数用意

--- Assets/Script/Enemy/EnemyData.cs
+++ Assets/Script/Enemy/EnemyData.cs
@@ -1,11 +1,19 @@
+using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyData : MonoBehaviour
 {
+    public float enemy_test_hp;
+    public float enemy_test_attack;
+    public float enemy_test_spped;
+    public float enemy_test_detect;
+    public float enemy_test_arm;
+    public float enemy_test_cooltime;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
