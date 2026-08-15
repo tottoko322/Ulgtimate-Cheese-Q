80942f4f2ea335230020028a4d42be060b160be2
k2008siru-star <k2008siru@gmail.com>
2026-08-15 17:01:03 +0900

    enemyData.csの修正

--- Assets/Script/Enemy/EnemyData.cs
+++ Assets/Script/Enemy/EnemyData.cs
@@ -6,32 +6,22 @@ public class EnemyData : MonoBehaviour
 {
     public float enemyHp;
     public float enemyAttack;
-    public float enemySpped;
-    public float enemyDetect;
-    public float enemyArm;
+    public float enemySpeed;
+    [SerializeField] float enemySearchRange;
+    public float enemyChaseRange;
+    public float enemyAttackRange;
     public float enemyCooltime;
-    public bool isInDetect;
-    public GameObject player;
-    private float distanceX;
-    private float distanceY;
+    public bool isInSearchRange;
+    public bool isInChaseRange;
+    public float distanceX;
+    public float distanceY;
+    [SerializeField] Sprite[] moveAnimationSprites;
+    [SerializeField] Sprite[] attackAnimationSprites;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        isInDetect = false;
+        isInSearchRange = false;
+        isInChaseRange = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //索敵範囲内かの判定
-        if(!isInDetect)
-        {
-            distanceX = transform.position.x - player.transform.position.x;
-            distanceY = transform.position.y - player.transform.position.y;
-            if(distanceX*distanceX + distanceY*distanceY < enemyDetect*enemyDetect)
-            {
-                isInDetect = true;
-            }
-        }
-    }
 }
