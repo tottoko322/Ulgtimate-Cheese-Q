1bac1b3dc05037253e4872c23d2102d9a778f667
k2008siru-star <k2008siru@gmail.com>
2026-08-19 13:14:44 +0900

    distance修正2

--- Assets/Script/Enemy/EnemyController.cs
+++ Assets/Script/Enemy/EnemyController.cs
@@ -5,10 +5,7 @@ public class EnemyController : MonoBehaviour
     EnemyData dataofenemy;
     [SerializeField] Transform playertransform;
     private bool isInSearchRange;
-    private bool isInChaseRange;
-    private float distanceX;
-    private float distanceY;
-
+    private bool isInChaseRange;    private float distanceX;
     void Start()
     {
         isInSearchRange = false;
@@ -27,23 +24,16 @@ public class EnemyController : MonoBehaviour
     {
         if(!isInSearchRange)
         {
-            distanceX = playertransform.position.x - transform.position.x;
-            distanceY = playertransform.position.y - transform.position.y;
-            if(distanceX*distanceX + distanceY*distanceY < dataofenemy.enemySearchRange*dataofenemy.enemySearchRange)
+            if(Vector2.Distance(playertransform.position,transform.position) < dataofenemy.enemySearchRange)
             {
                 isInSearchRange = true;
                 isInChaseRange = true;
             }
         }
-        else
+        else if(Vector2.Distance(playertransform.position,transform.position) < dataofenemy.enemySearchRange)
         {
-            distanceX = playertransform.position.x - transform.position.x;
-            distanceY = playertransform.position.y - transform.position.y;
-            if(distanceX*distanceX + distanceY*distanceY < dataofenemy.enemySearchRange*dataofenemy.enemySearchRange)
-            {
-                isInSearchRange = false;
-                isInChaseRange = false;
-            }
+            isInSearchRange = false;
+            isInChaseRange = false;
         }
     }
     void ChasePlayer()
