3b0f2aa1adeb214323d6e255e0b545e33cbb46cf
k2008siru-star <k2008siru@gmail.com>
2026-08-19 13:17:32 +0900

    Revert "distance修正2"
    This reverts commit 1bac1b3dc05037253e4872c23d2102d9a778f667.

--- Assets/Script/Enemy/EnemyController.cs
+++ Assets/Script/Enemy/EnemyController.cs
@@ -5,7 +5,10 @@ public class EnemyController : MonoBehaviour
     EnemyData dataofenemy;
     [SerializeField] Transform playertransform;
     private bool isInSearchRange;
-    private bool isInChaseRange;    private float distanceX;
+    private bool isInChaseRange;
+    private float distanceX;
+    private float distanceY;
+
     void Start()
     {
         isInSearchRange = false;
@@ -24,16 +27,23 @@ public class EnemyController : MonoBehaviour
     {
         if(!isInSearchRange)
         {
-            if(Vector2.Distance(playertransform.position,transform.position) < dataofenemy.enemySearchRange)
+            distanceX = playertransform.position.x - transform.position.x;
+            distanceY = playertransform.position.y - transform.position.y;
+            if(distanceX*distanceX + distanceY*distanceY < dataofenemy.enemySearchRange*dataofenemy.enemySearchRange)
             {
                 isInSearchRange = true;
                 isInChaseRange = true;
             }
         }
-        else if(Vector2.Distance(playertransform.position,transform.position) < dataofenemy.enemySearchRange)
+        else
         {
-            isInSearchRange = false;
-            isInChaseRange = false;
+            distanceX = playertransform.position.x - transform.position.x;
+            distanceY = playertransform.position.y - transform.position.y;
+            if(distanceX*distanceX + distanceY*distanceY < dataofenemy.enemySearchRange*dataofenemy.enemySearchRange)
+            {
+                isInSearchRange = false;
+                isInChaseRange = false;
+            }
         }
     }
     void ChasePlayer()
