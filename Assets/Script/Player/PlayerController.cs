0e95a0b83e4657a0c4c4566bf53b4cd208099cbf
TaiponQ <hentaipon3105@gmail.com>
2026-08-23 00:34:34 +0900

    a

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -10,17 +10,26 @@ public class PlayerController : MonoBehaviour
         DoubleJump,
         WallCling
     } 
+
     private LocomotionState jumpState = LocomotionState.Grounded;
     public LocomotionState CurrentLocomotionState { get; private set; } 
 
     [Header("Ground Movement")] 
-    [SerializeField] private float groundMoveSpeed = 10f;
+    [SerializeField] private float groundMoveSpeed = 8f;
 
     [Header("Jump")]
     [SerializeField] private float jumpPower = 5f; 
     [SerializeField] private float airJumpPower = 5f;
     [SerializeField] private float maxJumpHoldTime = 0.2f;
     private float jumpHoldTimer = 0f;
+
+    [Header("Air Movement")]
+    [SerializeField] private float airMoveSpeed = 8f; 
+    [SerializeField] private float airAcceleration = 3f; 
+    [SerializeField] private float airDeceleration; 
+
+    [Header("Fast Fall")]
+    [SerializeField] private float fastFallSpeed = 8f;
 
     private bool jumpPressed = false;
 
@@ -35,6 +44,8 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Jump();
+        HandleAirMovement();
+        StartFastFall();
     }
 
     void FixedUpdate()
@@ -44,28 +55,69 @@ public class PlayerController : MonoBehaviour
 
     private void HandleGroundedMovement()
     {
-        int playerState = 5;
-        if (Keyboard.current.dKey.isPressed)
+        if (jumpState == LocomotionState.Grounded)
         {
-            playerState += 1;
-        }
-        if (Keyboard.current.aKey.isPressed)
-        {
-            playerState -= 1;
-        }
+            int playerState = 5;
+            if (Keyboard.current.dKey.isPressed)
+            {
+                playerState += 1;
+            }
+            if (Keyboard.current.aKey.isPressed)
+            {
+                playerState -= 1;
+            }
 
-        switch(playerState)
-        {
-            case 4:
-            rb.linearVelocity = new Vector2(-groundMoveSpeed, rb.linearVelocity.y);
-            break;
-            case 5:
-            rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
-            break;
-            case 6:
-            rb.linearVelocity = new Vector2(groundMoveSpeed, rb.linearVelocity.y);
-            break;
+            switch(playerState)
+            {
+                case 4:
+                rb.linearVelocity = new Vector2(-groundMoveSpeed, rb.linearVelocity.y);
+                break;
+
+                case 5:
+                rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
+                break;
+
+                case 6:
+                rb.linearVelocity = new Vector2(groundMoveSpeed, rb.linearVelocity.y);
+                break;
+            }
         }
+    }
+
+    private void HandleAirMovement()
+    {
+        if (jumpState == LocomotionState.Airborne || jumpState == LocomotionState.DoubleJump)
+        {
+            int playerAirState = 5;
+            if (Keyboard.current.dKey.isPressed)
+            {
+                playerAirState += 1;
+            }
+            if (Keyboard.current.aKey.isPressed)
+            {
+                playerAirState -= 1;
+            }
+            switch(playerAirState)
+            {
+                case 4:
+                rb.linearVelocity = new Vector2(Mathf.MoveTowards(rb.linearVelocity.x, -airMoveSpeed, airAcceleration*Time.fixedDeltaTime), rb.linearVelocity.y);
+                break;
+
+                case 5:
+                rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
+                break;
+
+                case 6:
+                rb.linearVelocity = new Vector2(Mathf.MoveTowards(rb.linearVelocity.x, airMoveSpeed, airAcceleration*Time.fixedDeltaTime), rb.linearVelocity.y);
+                break;
+            }
+        }
+    }
+
+    private void StartFastFall()
+    {
+        if (Keyboard.current.sKey.wasPressedThisFrame && (jumpState == LocomotionState.Airborne || jumpState == LocomotionState.DoubleJump))
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Min(rb.linearVelocity.y, -fastFallSpeed));
     }
 
     private void Jump()
