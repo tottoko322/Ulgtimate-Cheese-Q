0d0da1a59a12d8609ffcd9e5af36a137cf31da75
tottoko322 <mercury8ly@icloud.com>
2026-09-01 16:55:01 +0900

    CreateNewSave()
    セーブデータのもとを作成

--- Assets/Script/core/SaveManager.cs
+++ Assets/Script/core/SaveManager.cs
@@ -2,15 +2,10 @@ using UnityEngine;
 
 public class SaveManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public SaveData SaveData { get; private set; }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateNewSave()
     {
-        
+        SaveData = new SaveData();
     }
 }
