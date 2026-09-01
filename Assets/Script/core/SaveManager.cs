d54b255724d59304d3e8a71543274e89dad4fded
tottoko322 <mercury8ly@icloud.com>
2026-09-01 17:07:34 +0900

    Save()関数作成

--- Assets/Script/core/SaveManager.cs
+++ Assets/Script/core/SaveManager.cs
@@ -1,11 +1,23 @@
 using UnityEngine;
+using System.IO;
 
 public class SaveManager : MonoBehaviour
 {
     public SaveData SaveData { get; private set; }
+    private string saveFilePath;
+    private void Awake()
+    {
+        saveFilePath = Path.Combine(Application.persistentDataPath, "save.json");
+    }
+    public void Save()
+    {
+        string json = JsonUtility.ToJson(SaveData);
+        File.WriteAllText(saveFilePath, json);
+    }
 
     private void CreateNewSave()
     {
         SaveData = new SaveData();
     }
+
 }
