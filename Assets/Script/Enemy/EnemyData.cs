74f9e554cfdde2c352733455e85540d9d9eb50f4
k2008siru-star <k2008siru@gmail.com>
2026-09-04 14:42:36 +0900

    プロパティ化

--- Assets/Script/Enemy/EnemyData.cs
+++ Assets/Script/Enemy/EnemyData.cs
@@ -3,19 +3,19 @@ using UnityEngine;
 public class EnemyData : MonoBehaviour
 {
     //能力値
-    public float enemyHp;
-    public float enemyAttack;
-    public float enemySpeed;
-    public float enemySearchRange;
-    public float enemyChaseRange;
-    public float enemyAttackRange;
-    public float enemyCooltime;
-    public float enemyFixedTime;
+    public float enemyHp{get;private set;}
+    public float enemyAttack{get;private set;}
+    public float enemySpeed{get;private set;}
+    public float enemySearchRange{get;private set;}
+    public float enemyChaseRange{get;private set;}
+    public float enemyAttackRange{get;private set;}
+    public float enemyCooltime{get;private set;}
+    public float enemyFixedTime{get;private set;}
     //アニメーション設定
-    public float enemyChangeMoveSpritesInterval;
-    public float enemyChangeAttackSpritesInterval;
-    public float enemyChangeStaySpritesInterval;
-    public Sprite[] enemyMoveAnimationSprites;
-    public Sprite[] enemyAttackAnimationSprites;
-    public Sprite[] enemyStayAnimationSprites;
+    public float enemyChangeMoveSpritesInterval{get;private set;}
+    public float enemyChangeAttackSpritesInterval{get;private set;}
+    public float enemyChangeStaySpritesInterval{get;private set;}
+    public Sprite[] enemyMoveAnimationSprites{get;private set;}
+    public Sprite[] enemyAttackAnimationSprites{get;private set;}
+    public Sprite[] enemyStayAnimationSprites{get;private set;}
 }
